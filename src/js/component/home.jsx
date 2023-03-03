bcae525fe5345e4046a911790ede59d214b9078a
phorjax <joseph.12082@gmail.com>
2023-03-03 02:06:05 +0000

    finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,12 +1,51 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("");
+	const [todo, setTodo] = useState([]);
+	
+
 	return (
-		<div></div>
+		<div className="container">
+			<h1>My to-dos</h1>
+			<ul>
+				<li> <input type="text" 
+				onChange={(e) => setInputValue(e.target.value)}
+				value={inputValue}
+				onKeyUp = {(e) => {
+					if (e.key === 'Enter') {
+						setTodo(todo.concat([inputValue]));
+						setInputValue("");
+					}
+				}}
+				placeholder="add to the to do list"></input>
+				</li>
+				
+					{todo.map((item, index) => (
+						<li>{item}{" "}
+						<i class="fa-solid fa-trash-can" onClick={() =>
+							 setTodo(
+								todo.filter(
+									(t, currentIndex) => 
+									index != currentIndex
+								)
+							)
+						}></i>
+						
+						
+						
+						</li>
+
+
+					))}
+			</ul>
+			
+
+
+		</div>
 	);
 };
 

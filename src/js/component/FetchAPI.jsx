3f7396131bace94ab10925ca259b85ff042bc710
phorjax <joseph.12082@gmail.com>
2023-03-08 22:33:40 +0000

    almost there

--- src/js/component/FetchAPI.jsx
+++ src/js/component/FetchAPI.jsx
@@ -1,53 +1,11 @@
 import React, { useState } from 'react';
 import Home from "./home.jsx";
 
-function FetchAPI() {
-  const [deleteResult, setDeleteResult] = useState(null)
-    const formatResponse = (res) => {
-    return JSON.stringify(res, null)
-    }
 
-  const apiGet = () => {
-    fetch('https://3000-phorjax-todolistreact-912wcjrntfk.ws-us89.gitpod.io/', {
-      method: "GET",
-      // body: JSON.stringify(todo),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-    .then(resp => {
-        console.log(resp.ok); // will be true if the response is successfull
-        console.log(resp.status); // the status code = 200 or code = 400 etc.
-        console.log(resp.text()); // will try return the exact result as string
-        return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
-    })
-    .then(data => {
-        //here is were your code should start after the fetch finishes
-        console.log(data); //this will print on the console the exact object received from the server
-    })
-    .catch(error => {
-        //error handling
-        console.log(error);
-    });
-  }
-  async function deleteAllInputs() {
-    try{
-      const res = await fetch(`${"https://3000-phorjax-todolistreact-912wcjrntfk.ws-us89.gitpod.io/"}`, {method: "DELETE"}) 
-      const data = await res.json()
-      const result = {
-        status: res.status + "-" + res.statusText,
-        headers: {"Content-Type": res.headers.get("Content-Type")},
-        data: data
-      }
-      setDeleteResult(formatResponse(result))
-    }
-    catch(err){
-      setDeleteResult(err.message)
-    }
-  }
-  return (
-    <button onClick={deleteAllInputs()} > my API</button>
-    )
-}
+  
+    
 
-export default FetchAPI
+  
+ 
+
+
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,53 +1,84 @@
-import React, {useState} from "react";
-import FetchAPI from "./FetchAPI.jsx";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
 
 //create your first component
 const Home = () => {
-	const [inputValue, setInputValue] = useState("");
-	const [todo, setTodo] = useState([]);
-	
+  const [inputValue, setInputValue] = useState("");
+  const [todo, setTodo] = useState([]);
 
-	return (
-		<div className="container">
-			<h1>My to-dos</h1>
-			<ul>
-				<li> <input type="text" 
-				onChange={(e) => setInputValue(e.target.value)}
-				value={inputValue}
-				onKeyUp = {(e) => {
-					if (e.key === 'Enter') {
-						setTodo(todo.concat([inputValue]));
-						setInputValue("");
-					}
-				}}
-				placeholder="add to the to do list"></input>
-				</li>
-				
-					{todo.map((item, index) => (
-						<li>{item}{" "}
-						<i class="fa-solid fa-trash-can" onClick={() =>
-							 setTodo(
-								todo.filter(
-									(t, currentIndex) => 
-									index != currentIndex
-								)
-							)
-						}></i>
-						
-						
-						
-						</li>
+  let apiUrl = "https://assets.breatheco.de/apis/fake/todos/user/phorjax";
+  const apiGet = () => {
+    fetch(apiUrl)
+      .then((resp) => resp.json())
+      .then((data) => setTodo(data))
+      .catch((error) => console.log(error));
+  }
 
+  useEffect(() => {
+    apiGet();
+  }, []);
 
-					))}
-			</ul>
-			<div><p>{todo.length} tasks</p></div>
+ function clearAllTasks() {
+	let newTodo =   {
+        label: "sample task",
+        done: false
+    };	
+	fetch(apiUrl,
+		{
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+			body: JSON.stringify(newTodo)
+        }
+    )
+	.then((resp) => console.log( resp.json()))
+	.then(()=>setTodo([]))
+	.catch((error) =>console.log(error))
+  }
 
-			<div><FetchAPI /></div>
-		</div>
-	);
+  return (
+    <div className="container">
+      <h1>My to-dos</h1>
+      <ul>
+        <li>
+          {" "}
+          <input
+            type="text"
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            onKeyUp={(e) => {
+              if (e.key === "Enter") {
+                setTodo(todo.concat([inputValue]));
+                setInputValue("");
+              }
+            }}
+            placeholder="add to the to do list"
+          ></input>
+        </li>
+
+        {todo.map((item, index) => (
+          <li>
+            {item.label}{" "}
+            <i
+              class="fa-solid fa-trash-can"
+              onClick={() =>
+                setTodo(todo.filter((t, currentIndex) => index != currentIndex))
+              }
+            ></i>
+          </li>
+        ))}
+      </ul>
+      <div>
+        <p>{todo.length} tasks</p>
+      </div>
+
+      <div>
+        <button onClick={()=>clearAllTasks()}> Clear all tasks</button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
